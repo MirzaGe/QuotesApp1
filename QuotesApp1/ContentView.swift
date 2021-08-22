37b5c283ed02ddf3b6d2d54d1698edcf3138164f
Sherry1481 <shareyscorpi1@gmail.com>
2021-08-22 21:29:23 +0500

    Initial Commit

---
+++ QuotesApp1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuotesApp1
+//
+//  Created by sherry on 22/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuotesApp1/QuotesApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp1App.swift
+//  QuotesApp1
+//
+//  Created by sherry on 22/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
