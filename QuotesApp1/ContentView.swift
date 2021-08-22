ded4a65aa3c291ab28bc1493fa98b55a53aef777
Sherry1481 <shareyscorpi1@gmail.com>
2021-08-22 22:24:04 +0500

    commit

--- QuotesApp1/ContentView.swift
+++ QuotesApp1/ContentView.swift
@@ -9,10 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       MainView()
     }
 }
+
+struct MainView: View {
+    let quotes = ["quotes1","quotes2","quotes2","quotes2","quotes2","quotes2","quotes2","quotes2","quotes2","quotes2","quotes2","quotes2","quotes2","quotes2","quotes2","quotes2"]
+    
+    var body: some View {
+        VStack {
+            
+            HStack {
+                Text("Quotes Available")
+                    .font(.subheadline)
+                    .italic()
+                    .foregroundColor(.orange)
+            }
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(self.quotes, id:\.self) {
+                        quote in
+                        VStack {
+                            Image("lilly")
+                                .resizable()
+                                .clipShape(Circle())
+                                .overlay(Circle().stroke(Color.gray,lineWidth: 2))
+                                
+                                .frame(width: 60, height: 60)
+                                Text(quote)
+                                .font(.headline)
+                        }
+                        
+                       
+                    }
+                }
+            }
+            
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
